199b415b6309dcb2e403f1ea6a982534e44e6463
Craig <craigjope@gmail.com>
2021-08-21 13:15:09 -0600

    commit tree cfec5c40d263081d0175d943cbebff286ccea411

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -3,9 +3,14 @@ import React from "react";
 function Tickler() {
   function tickle() {
     console.log("Teehee!");
+    
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
+
+  //as an arrow function:
+  // return <button onClick={()=>{console.log("Teehee")}}>Tickle me!</button>;
+
 }
 
 export default Tickler;
